469fb547d0300215c1eefa1849c371d47725c0de
boming <boming0529@gmail.com>
2023-11-19 09:15:50 +0800

    [UPD] 參數修改和備註修改
    1. 備註增加課本的公式
    2. 備註增加英文跟部分修改成繁中
    3. 將原始回波修改為三個目標物
    4. matched filter 改用加窗函式
    5. 圖片橫軸縱軸和標頭文字修改

--- 1-SAR成像算法/RD算法/2-2014.10.10. 小斜视角，RDA/RDA_imaging.m
+++ 1-SAR成像算法/RD算法/2-2014.10.10. 小斜视角，RDA/RDA_imaging.m
@@ -1,45 +1,51 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%            小斜视角（3.5°）
+%            小斜視角（3.5°）
 %                RDA
+%      RDA in the Low Squint Case
 %              点目标仿真
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 截止到2014.10.10. 17:19 p.m.
+% update Nov 18 2023. 
 
 %%
 clear;
 close all;
 clc;
 % --------------------------------------------------------------------
-% 定义参数
+% Table 6.1 : C-Band AirBorn SAR Parameters Used in the Simulations
 % --------------------------------------------------------------------
-R_nc = 20e3;                % 景中心斜距
-Vr = 150;                   % 雷达有效速度
-Tr = 2.5e-6;                % 发射脉冲时宽
-Kr = 20e12;                 % 距离调频率
-f0 = 5.3e9;                 % 雷达工作频率
+R_nc = 20e3;                % 景中心斜距 (Slant range of scene center, Km)
+Vr = 150;                   % 雷達有效速度 (Effective rader velocity, m/s)
+Tr = 2.5e-6;                % 發射脈衝時寬 (Transmitted pulse duration, \mu s)
+Kr = 20e12;                 % 距離向調频率 (Range FM rate )
+f0 = 5.3e9;                 % 雷達工作頻率 ()
 BW_dop = 80;                % 多普勒带宽
 Fr = 60e6;                  % 距离采样率
-Fa = 200;                   % 方位采样率
-Naz = 1024;                 % 距离线数（即数据矩阵，行数）——这里修改为1024。
+Fa = 100;                   % 方位采样率
+Naz = 256;                  % 距离线数（即数据矩阵，行数）——这里修改为1024。
 Nrg = 320;                  % 距离线采样点数（即数据矩阵，列数）
 sita_r_c = (3.5*pi)/180;	% 波束斜视角，3.5 度，这里转换为弧度
+
+% --------------------------------------------------------------------
+% 定義參數
+% --------------------------------------------------------------------
 c = 3e8;                    % 光速
 
-R0 = R_nc*cos(sita_r_c);	% 与R_nc相对应的最近斜距，记为R0
+R0 = R_nc*cos(sita_r_c);	% 与R_nc相对应的最近斜距，记为R0, (4.17)
 Nr = Tr*Fr;                 % 线性调频信号采样点数
-BW_range = Kr*Tr;           % 距离向带宽
+BW_range = Kr*Tr;           % 距离向带宽, Page 94. 根據表 (4.1)
 lamda = c/f0;               % 波长
 fnc = 2*Vr*sin(sita_r_c)/lamda;     % 多普勒中心频率，根据公式（4.33）计算。
-La_real = 0.886*2*Vr*cos(sita_r_c)/BW_dop; % 方位向天线长度，根据公式（4.36）
-beta_bw = 0.886*lamda/La_real;              % 雷达3dB波束     
-La = 0.886*R_nc*lamda/La_real;              % 合成孔径长度
-a_sr = Fr / BW_range;       % 距离向过采样因子
-a_sa = Fa / BW_dop;         % 方位向过采样因子
+La_real = 0.886*2*Vr*cos(sita_r_c)/BW_dop; % 方位向天线长度，根据公式（4.36）or 表 (4.1)
+beta_bw = 0.886*lamda/La_real;              % 雷达3dB波束 (Page 86. 方位角波束寬度)    
+La = 0.886*R_nc*lamda/La_real;              % 合成孔径长度 (Page 86. 方位角波束寬度在地面上的投影) or 表 (4.1) 方位角波束覆蓋區
+a_sr = Fr / BW_range;       % 距离向过采样因子 (4.22)
+a_sa = Fa / BW_dop;         % 方位向过采样因子 , 有可能在 5.48~5.49
 
-Mamb = round(fnc/Fa);       % 多普勒模糊
+Mamb = round(fnc/Fa);       % 多普勒模糊 (12.20) 有點不同
 
 NFFT_r = Nrg;               % 距离向FFT长度
-NFFT_a = Naz;               % 方位向FFT长度
+NFFT_a = Naz;               % 方位向FFT长度 (與表 6.1 不同)
 
 % --------------------------------------------------------------------
 % 设定仿真点目标的位置
@@ -82,7 +88,7 @@ ta = ( -Naz/2: Naz/2-1 )/Fa;                            % 方位时间轴
 fa = fnc + ( -NFFT_a/2 : NFFT_a/2-1 )*( Fa/NFFT_a );	% 方位频率轴
 
 % 生成距离（方位）时间（频率）矩阵
-tr_mtx = ones(Naz,1)*tr;    % 距离时间轴矩阵，大小：Naz*Nrg
+tau_mtx = ones(Naz,1)*tr;    % 距离时间轴矩阵，大小：Naz*Nrg
 ta_mtx = ta.'*ones(1,Nrg);  % 方位时间轴矩阵，大小：Naz*Nrg
 
 %% 
@@ -92,9 +98,9 @@ ta_mtx = ta.'*ones(1,Nrg);  % 方位时间轴矩阵，大小：Naz*Nrg
 s_echo = zeros(Naz,Nrg);    % 用来存放生成的回波数据
 
 A0 = 1;                     % 目标回波幅度，都设置为1.
-for k = 1:1                 % 生成k个目标的原始回波数据
-    R_n = sqrt( (x_range(k).*ones(Naz,Nrg)).^2 + (Vr.*ta_mtx-y_azimuth(k).*ones(Naz,Nrg)).^2 );% 目标k的瞬时斜距
-    w_range = ((abs(tr_mtx-2.*R_n./c)) <= ((Tr/2).*ones(Naz,Nrg)));     % 距离向包络，即距离窗
+for k = 1:3                 % 生成k个目标的原始回波数据
+    R_n = sqrt( (x_range(k).*ones(Naz,Nrg)).^2 + (Vr.*ta_mtx-y_azimuth(k).*ones(Naz,Nrg)).^2 );% 目标k的瞬时斜距 (6.2)
+    w_range = ((abs(tau_mtx-2.*R_n./c)) <= ((Tr/2).*ones(Naz,Nrg)));     % 距离向包络，即距离窗
     % =====================================================================    
     % 方位向包络，也就是 天线的双程方向图作用因子。
     %{
@@ -111,8 +117,8 @@ for k = 1:1                 % 生成k个目标的原始回波数据
     w_azimuth = w_azimuth.'*ones(1,Nrg);    % 生成Naz*Nrg的矩阵
     %}
 % =====================================================================     
-    s_k = A0.*w_range.*w_azimuth.*exp(-(1j*4*pi*f0).*R_n./c).*exp((1j*pi*Kr).*(tr_mtx-2.*R_n./c).^2);
-%     s_k = A0.*w_range.*w_azimuth1.*w_azimuth.*exp(-(1j*4*pi*f0).*R_n./c).*exp((1j*pi*Kr).*(tr_mtx-2.*R_n./c).^2);
+    s_k = A0.*w_range.*w_azimuth.*exp(-(1j*4*pi*f0).*R_n./c).*exp((1j*pi*Kr).*(tau_mtx-2.*R_n./c).^2);  % (6.1)
+%     s_k = A0.*w_range.*w_azimuth1.*w_azimuth.*exp(-(1j*4*pi*f0).*R_n./c).*exp((1j*pi*Kr).*(tau_mtx-2.*R_n./c).^2);
     % 上式就是生成的某一个点目标（目标k）的回波信号。
     % 经过几次循环，生成几个点目标的回波信号，相加即可。
     if k == 1
@@ -133,29 +139,29 @@ end
 figure;
 subplot(2,2,1);
 imagesc(real(s_echo));
-title('（a）实部');
-xlabel('距离时域（采样点）');
-ylabel('方位时域（采样点）');
-text(300,-70,'图1，原始数据');       % 给图1进行文字说明 
+title('(a) 實部 Real Part');
+xlabel('Range Time Domain(Samples)');
+ylabel('Azimuth Time Domain(Samples)');
+text(280,-150,'圖一: 目標物 A 原始數據');       % 给图1进行文字说明 
 % text 函数：在图像的指定坐标位置，添加文本框
 
 subplot(2,2,2);
 imagesc(imag(s_echo));
-title('（b）虚部');
-xlabel('距离时域（采样点）');
-ylabel('方位时域（采样点）');
+title('(b) 虚部 Imaginary Part');
+xlabel('Range Time Domain(Samples)');
+ylabel('Azimuth Time Domain(Samples)');
 
 subplot(2,2,3);
 imagesc(abs(s_echo));
-title('（c）幅度');
-xlabel('距离时域（采样点）');
-ylabel('方位时域（采样点）');
+title('(c) 強度 Magnitude');
+xlabel('Range Time Domain(Samples)');
+ylabel('Azimuth Time Domain(Samples)');
 
 subplot(2,2,4);
 imagesc(angle(s_echo));
-title('（d）相位');
-xlabel('距离时域（采样点）');
-ylabel('方位时域（采样点）');
+title('(d) 相位 Phase');
+xlabel('Range Time Domain(Samples)');
+ylabel('Azimuth Time Domain(Samples)');
 % colormap(gray);
 
 
@@ -176,7 +182,7 @@ title('二维频谱相位');
 
 %%
 % --------------------------------------------------------------------
-% 距离压缩
+% 6.3.2 Range Compression
 % --------------------------------------------------------------------
 S_range = fft(s_echo,NFFT_r,2);     % 进行距离向傅里叶变换，零频在两端。
 
@@ -186,20 +192,20 @@ S_range = fft(s_echo,NFFT_r,2);     % 进行距离向傅里叶变换，零频在
 figure;
 subplot(1,2,1);
 imagesc(real(S_range));
-title('（a）实部');
-xlabel('距离频域（采样点）');
-ylabel('方位时域（采样点）');
-text(280,-60,'图2，距离频域');       % 给图2进行文字说明
-text(340,-10,'未压缩');       
+title('(a)實部');
+xlabel('Range frequency Domain(Samples)');
+ylabel('Azimuth time Domain(Samples)');
+text(280,-60,'圖二: Range frequency Domain');       % 给图2进行文字说明
+text(340,-10,'未壓縮');       
 
 subplot(1,2,2);
 imagesc(abs(S_range));
-title('（b）幅度');
-xlabel('距离频域（采样点）');
-ylabel('方位时域（采样点）');
+title('(b)幅度');
+xlabel('Range frequency Domain(Samples)');
+ylabel('Azimuth time Domain(Samples)');
 %}
 
-%　生成距离向匹配滤波器
+%　生成距离向匹配滤波器 (matched filter)
 % ====================================================
 % 采用方式2
 % 时域复制脉冲，末端补零，fft，再取复共轭。
@@ -208,14 +214,17 @@ t_ref_mtx = ones(Naz,1)*t_ref;      % 矩阵形式
 w_ref = kaiser(Nr,2.5);             % 距离向，构建Kaiser窗，此为列向量。
 w_ref = ones(Naz,1)*(w_ref.');      % 构成矩阵形式，每一行都相同的加窗。
 
-s_ref = exp((1j*pi*Kr).*((t_ref_mtx).^2)); % 复制（发射）脉冲，未加窗。
-% s_ref = w_ref.*exp((1j*pi*Kr).*((t_ref_mtx).^2)); % 复制（发射）脉冲，加了窗。
+% s_ref = exp((1j*pi*Kr).*((t_ref_mtx).^2)); % 复制（发射）脉冲，未加窗。
+s_ref = w_ref.*exp((1j*pi*Kr).*((t_ref_mtx).^2)); % 复制（发射）脉冲，加了窗 (3.48)。
 
+% region : (3.49)
 s_ref = [s_ref,zeros(Naz,Nrg-Nr)];      % 对复制脉冲，后端补零。
 S_ref = fft(s_ref,NFFT_r,2);            % 复制脉冲的距离傅里叶变换，零频在两端。
 H_range = conj(S_ref);                  % 距离向匹配滤波器，零频在两端。
+% endregion 
+
 S_range_c = S_range.*H_range;           % 乘以匹配滤波器，零频在两端。    
-s_rc = ifft(S_range_c,[],2);            % 完成距离压缩，回到二维时域。
+s_rc = ifft(S_range_c,[],2);            % 完成距离压缩，回到二维时域。 (6.3)
 % s_rc的长度为：Naz*Nrg。未去除弃置区。
 
 % 对s_rc进行去除弃置区的操作
@@ -232,17 +241,17 @@ s_rc_c = s_rc(:,1:N_rg);                % 取前 N_rg列。
 figure;
 subplot(1,2,1);
 imagesc(real(S_range_c));
-title('（a）实部');
-xlabel('距离频域（采样点）');
-ylabel('方位时域（采样点）');
-text(280,-60,'图3，距离频域');       % 给图3进行文字说明
-text(340,-10,'已压缩');       
+title('(a)實部');
+xlabel('Range frequency Domain(Samples)');
+ylabel('Azimuth time Domain(Samples)');
+text(280,-60,'圖三: Range frequency Domain');       % 给图3进行文字说明
+text(340,-10,'已壓縮');       
 
 subplot(1,2,2);
 imagesc(abs(S_range_c));
-title('（b）幅度');
-xlabel('距离频域（采样点）');
-ylabel('方位时域（采样点）');
+title('(b)幅度');
+xlabel('Range frequency Domain(Samples)');
+ylabel('Azimuth time Domain(Samples)');
 %}
 %
 % 作图
@@ -250,25 +259,26 @@ ylabel('方位时域（采样点）');
 figure;
 subplot(1,2,1);
 imagesc(real(s_rc_c));  %　这及其以下，都直接使用去除弃置区后的结果
-title('（a）实部');
-xlabel('距离时域（采样点）');
-ylabel('方位时域（采样点）');
-text(150,-60,'图4，二维时域');       % 给图4进行文字说明
-text(172,-10,'完成压缩');       
+title('(a)實部');
+xlabel('Range Time Domain(Samples)');
+ylabel('Azimuth Time Domain(Samples)');
+text(130, -15,'圖四 : Range Compressed Data');       % 给图4进行文字说明
+% text(172,-10,'完成壓縮');       
 
 subplot(1,2,2);
 imagesc(abs(s_rc_c));
-title('（b）幅度');
-xlabel('距离时域（采样点）');
-ylabel('方位时域（采样点）');
+title('(b)幅度');
+xlabel('Range Time Domain(Samples)');
+ylabel('Azimuth Time Domain(Samples)');
 %}
 
 %%
 % --------------------------------------------------------------------
-% 变换到距离多普勒域，进行距离徙动校正
+% 6.3.4 RCMC
+% 变换到距离多普勒域，进行距离徙动校正 
 % --------------------------------------------------------------------
 s_rc_c = s_rc_c.*exp(-1j*2*pi*fnc.*(ta.'*ones(1,N_rg)));    % 数据搬移
-S_rd = fft(s_rc_c,NFFT_a,1);            % 方位向傅里叶变换，到距离多普勒域
+S_rd = fft(s_rc_c,NFFT_a,1);            % 方位向傅里叶变换，到距离多普勒域 (Azimuth FFT)
 % ====================================================================
 % 设置方位向频率轴
 fa = fnc + fftshift(-NFFT_a/2:NFFT_a/2-1)/NFFT_a*Fa;    % 方位频率轴如此设置。
@@ -292,7 +302,7 @@ for p = 1 : NFFT_a
         ii = ( Rrd_fn_p-(Rrd_fn_p_zheng-R/2):-1:Rrd_fn_p-(Rrd_fn_p_zheng+R/2-1)  );        
         rcmc_sinc = sinc(ii);
         rcmc_sinc = rcmc_sinc/sum(rcmc_sinc);   % 插值核的归一化
-        % ii 是sinc插值过程的变量;
+        % ii 是sinc插值过程的变量; (2.7)
         % g(x)=sum(h(ii)*g_d(x-ii)) = sum(h(ii)*g_d(ll));
                
         % 由于S_rd只有整数点取值，且范围有限。因此插值中要考虑它的取值溢出边界问题。
@@ -329,16 +339,16 @@ end
 figure;
 subplot(1,2,1);
 imagesc(real(S_rd));
-title('（a）实部');
-xlabel('距离时域（采样点）');
-ylabel('方位频域（采样点）');
-text(150,-60,'图5，距离多普勒域');       % 给图5进行文字说明
-text(172,-10,'未RCMC');       
+title('(a)實部');
+xlabel('Range time Domain(Samples)');
+ylabel('Azimuth frequency Domain(Samples)');
+text(130,-10,'圖五 : Range Dopper Domain');
+% text(172,-10,'未RCMC');
 subplot(1,2,2);
 imagesc(abs(S_rd));
-title('（b）幅度');
-xlabel('距离时域（采样点）');
-ylabel('方位频域（采样点）');
+title('(b)幅度');
+xlabel('Range time Domain(Samples)');
+ylabel('Azimuth frequency Domain(Samples)');
 
 % 作图
 % 图6——距离多普勒域，RCMC后的结果
@@ -391,7 +401,7 @@ NN = 20;
 % 目标1，点目标中心在 （ tg_1_x，tg_1_y ）
 tg_1_x = 96;
 tg_1_y = round(N_rg/2);
-target_1 = target_analysis_2( s_ac(tg_1_x-NN:tg_1_x+NN,tg_1_y-NN:tg_1_y+NN),Fr,Fa,Vr);
+% target_1 = target_analysis_2( s_ac(tg_1_x-NN:tg_1_x+NN,tg_1_y-NN:tg_1_y+NN),Fr,Fa,Vr);
 
 % 目标2，点目标中心在 （tg_2_x，target_2_y）
 tg_2_x = tg_1_x + delta_R1/Vr*Fa;
